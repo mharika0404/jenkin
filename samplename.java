39844ec3a2fb6a6e27be1d555bfa5e782d3947e1
mharika0404 <mharika0404@gmail.com>
2025-08-27 14:49:24 +0530

    Update samplename.java

--- samplename.java
+++ samplename.java
@@ -1,7 +1,7 @@
 public class samplename {
     public static void main(String[] args) {
         String name = "harika";
-        System.out.println("Hello, my name is " + name)
+        System.out.println("Hello, my name is " + name);
         
     }
 }
